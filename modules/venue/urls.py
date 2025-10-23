c5fffc242c2910534491956506af613fef51a0ac
rheinaadinda <moranisinurat@gmail.com>
2025-10-23 16:16:59 +0700

    simpan sebelum pop

--- modules/venue/urls.py
+++ modules/venue/urls.py
@@ -4,8 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
-app_name = 'search_venue'
+app_name = 'venue'
+
+from modules.venue.views import search_venue
 
 urlpatterns = [
-    path('', views.search_venue, name='search_venue'),
+    path('', search_venue, name='search_venue'),
+    path('venue/<int:venue_id>/', views.venue_detail, name='venue_detail'),
 ]
