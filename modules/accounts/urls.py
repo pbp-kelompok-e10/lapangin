7a608665f2ee7e0a128c1076030e812536c817b0
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2025-12-21 23:24:39 +0700

    add edit profile

--- modules/accounts/urls.py
+++ modules/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from modules.main.views import *
 from django.conf import settings
 from django.conf.urls.static import static
-from modules.accounts.views import login_user, register, logout_user, get_page_data, profile_page
+from modules.accounts.views import login_user, register, logout_user, get_page_data, profile_page, edit_profile
 
 app_name = 'accounts'
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('logout/', logout_user, name='logout'),
     path('page-data', get_page_data, name='get_page_data'),
-    path('profile/', profile_page, name='profile' )
+    path('profile/', profile_page, name='profile' ),
+    path('edit-profile/', edit_profile, name='edit_profile'),
 ]
